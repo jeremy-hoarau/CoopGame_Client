3a0553a5761c161c49b2cc1cd66745a3a07007ea
jeremy <jeremy.hoarau@supinfo.com>
2020-10-26 19:35:22 +0400

    Grapple Indicator

---
+++ Assets/Scripts/Players/GrappleIndicator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+public class GrappleIndicator : MonoBehaviour
+{
+    public GameObject indicator;
+
+    private PlayerManager playerManager;
+    private bool isInGrappleRange;
+    private Vector3 grapplePoint;
+
+    private void Start()
+    {
+        playerManager = GetComponent<PlayerManager>();
+    }
+
+    private void Update()
+    {
+        if (playerManager.isGrappling || !isInGrappleRange)
+        {
+            indicator.SetActive(false);
+        }
+        else if(isInGrappleRange && !indicator.activeSelf)
+        {
+            indicator.SetActive(true);
+        }
+
+        if (indicator.activeSelf)
+        {
+            indicator.transform.LookAt(grapplePoint, Vector3.down);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("GrapplePoint"))
+        {
+            isInGrappleRange = true;
+            grapplePoint = other.transform.position;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("GrapplePoint"))
+        {
+            isInGrappleRange = false;
+        }
+    }
+}
